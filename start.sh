5ae75e77ecdbdd1197fc094f07deda1a633b7c67
Joe Boylson <34290408+joeboylson@users.noreply.github.com>
2024-04-05 16:13:28 +0000

    build: use npm serve instead of Dockerfile;

--- start.sh
+++
@@ -1,2 +0,0 @@
-docker build -t joeboylsontech .
-docker run -dp 127.0.0.1:3000:80 joeboylsontech
--- stop.sh
+++
@@ -1,1 +0,0 @@
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=joeboylsontech --format="{{.ID}}"))
